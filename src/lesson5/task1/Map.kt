005bde244296db1e5d78fde68bafd7e0e0d3cb1f
Julia <tsrk.j@mail.ru>
2018-12-05 22:21:00 +0300

    Исправление 5, 6

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -362,39 +362,4 @@ fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
 //Задача решена на основе "рюкзака", взятого из Интернета с помощью метода
 // динамического программирования, так как преподователи разрешили пользоваться подобным.
 
-fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<String> {
-    val res = mutableSetOf<String>()
-    //создаю двумерный массив(вместимость рюкзака и вместившиеся предметы). Он больше treasures.size и capasity.size
-    // на 1 строчку и 1 столбец, т. к крайняя верхняя строчка capacity не имеет веса, так же,
-    // как и самый крайний столбец не имеет предметов
-    val maxCost = Array(treasures.size + 1) { Array(capacity + 1) { 0 } }
-    val weight = mutableListOf<Int>()
-    val price = mutableListOf<Int>()
-    val name = mutableListOf<String>()
-    // делю map на листы, мне удобнее так рассматривать его элементы
-    treasures.forEach {
-        weight.add(it.value.first)
-        price.add(it.value.second)
-        name.add(it.key)
-    }
-    for (i in 0 until capacity + 1) maxCost[0][i] = 0
-    for (i in 0 until treasures.size + 1) maxCost[0][i] = 0
-    for (k in 1 until treasures.size + 1) {
-        //опять же, прохожу с 1, так как края в двумерном массиве равны 0
-        for (s in 1 until capacity + 1) {
-            if (s >= weight[k - 1])
-                maxCost[k][s] = maxOf(maxCost[k - 1][s], maxCost[k - 1][s - weight[k - 1]] + price[k - 1])
-            else maxCost[k][s] = maxCost[k - 1][s]
-        }
-    }
-    fun findAns(k: Int, s: Int) {
-        if (maxCost[k][s] == 0) return
-        if (maxCost[k - 1][s] == maxCost[k][s]) findAns(k - 1, s)
-        else {
-            findAns(k - 1, s - weight[k - 1])
-            res.add(name[k - 1])
-        }
-    }
-    findAns(treasures.size, capacity)
-    return res
-}
+fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<String> = TODO()
